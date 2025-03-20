ad3ac354c3fac26e246db6173924602bf19069ff
choisieon <la-poderosa@naver.com>
2025-03-20 10:38:21 +0900

    migration

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField
+    created_at = models.DateTimeField(auto_now_add=True)
+    #파이썬 세상에서 sql세상으로 가기전에 migrations
